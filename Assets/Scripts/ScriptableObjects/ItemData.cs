7f67147ac0b255173ca660b3d3469199cc5f5616
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-03 20:26:55 +0900

    [feat] ConsumeSO 완성

--- Assets/Scripts/ScriptableObjects/ItemData.cs
+++ Assets/Scripts/ScriptableObjects/ItemData.cs
@@ -11,27 +11,6 @@ public class ItemData : ScriptableObject
     public GameObject dropPrefab;
 
     [Header("Stacking")]
-    public bool canStack;
-    public int maxStackAmount;
-}
-
-[System.Serializable]
-public class ItemDataConsumable
-{
-    public eConsumableType type;  // 철자 확인 (eConSumableType -> eConsumableType)
-    public float value;
-}
-
-[CreateAssetMenu(fileName = "ConsumableItem", menuName = "SO/New Consumable", order = 1)]
-public class ConsumableData : ItemData
-{
-    [Header("Consumable")]
-    public ItemDataConsumable[] consumables;
-}
-
-[CreateAssetMenu(fileName = "EquipItem", menuName = "SO/New Equip", order = 2)]
-public class EquipData : ItemData
-{
-    [Header("Equip")]
-    public GameObject equipPrefab;
+    public bool canStack = true;
+    public int maxStackAmount = 99;
 }
