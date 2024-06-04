959115e663c0b95c25eeedba7fe45f4510a5406b
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-04 12:16:48 +0900

    [feat] 장착할 장비아이템 구조화

---
+++ Assets/Scripts/Objects/Eqiup/Equip.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Equip : MonoBehaviour
+{
+    public virtual void OnEquip()
+    {
+        //장비 슬롯에 아이템 추가
+    }
+
+    public virtual void OnUnequip()
+    {
+         //장비 슬롯에 아이템 해제
+    }
+}
