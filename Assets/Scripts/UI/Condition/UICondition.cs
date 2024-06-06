d7f832d84edf68328ae60cb43325adf088951ca7
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-06 15:16:30 +0900

    [fix] 컴포넌트 자동연결

--- Assets/Scripts/UI/Condition/UICondition.cs
+++ Assets/Scripts/UI/Condition/UICondition.cs
@@ -10,6 +10,12 @@ public class UICondition : MonoBehaviour
 
     private void Start()
     {
+        hunger = transform.Find("CurrentValues").GetChild(0).GetComponent<Condition>();
+        thirst = transform.Find("CurrentValues").GetChild(1).GetComponent<Condition>();
+        health = transform.Find("CurrentValues").GetChild(2).GetComponent<Condition>();
+        stamina = transform.Find("CurrentValues").GetChild(3).GetComponent<Condition>();
+        temperature = transform.Find("CurrentValues").GetChild(4).GetComponent<Temperature>();
+
         CharacterManager.Instance.Player.condition.uiCondition = this;
     }
 }
