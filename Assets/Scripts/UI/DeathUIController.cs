25f12a50809c1557aeffa554107b42d2e384f325
boguen <qhrms012@naver.com>
2024-06-07 21:15:19 +0900

    [feat] DeathUIController

---
+++ Assets/Scripts/UI/DeathUIController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class DeathUIController : MonoBehaviour
+{
+    public CanvasGroup deathUICanvasGroup;
+    public float fadeDuration = 1.0f;
+
+
+    void Start()
+    {
+        deathUICanvasGroup.alpha = 0f;
+    }
+
+    public void ShowDeathUI()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            deathUICanvasGroup.alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
+            yield return null;
+        }
+        deathUICanvasGroup.alpha = 1.0f;
+    }
+}
