796aa7a453bc64d7555e7a9460a58640bd09f4b5
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-10 18:29:44 +0900

    [refactor] 스타트신 리팩토링+GameManager 시간흐름이 시작할 떄마다 초기화되도록 조정

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 
 public class DataManager : Singleton<DataManager>
