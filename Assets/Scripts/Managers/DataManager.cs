9d3109ad84720f957b49a7058fc3b85e98574fbb
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-10 19:30:01 +0900

    [remove] Datamanager에서 퀘스트 관련 코드 삭제

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -6,7 +6,6 @@ public class DataManager : Singleton<DataManager>
     public Dictionary<string, ItemData> itemDataDictionary;
     public Dictionary<string, GameObject> itemPrefabDictionary;
     public Dictionary<string, GameObject> EquipPrefabDictionary;
-    public Dictionary<string, QuestData> QuestDataDictionary;
 
     protected override void Awake()
     {
@@ -14,7 +13,6 @@ public class DataManager : Singleton<DataManager>
         itemDataDictionary = new Dictionary<string, ItemData>();
         itemPrefabDictionary = new Dictionary<string, GameObject>();
         EquipPrefabDictionary = new Dictionary<string, GameObject>();
-        QuestDataDictionary = new Dictionary<string, QuestData>();
 
 
         foreach (var data in Resources.LoadAll<ItemData>("Item_SOs"))
@@ -29,27 +27,5 @@ public class DataManager : Singleton<DataManager>
         {
             EquipPrefabDictionary.Add(data.name, data);
         }
-        foreach (var data in Resources.LoadAll<QuestData>("Quest_SO"))
-        {
-            QuestDataDictionary.Add(data.name, data);
-        }
-
-        Debug.Log("QuestDataDictionary 초기화 완료:");
-        foreach (var quest in QuestDataDictionary.Values)
-        {
-            Debug.Log($"Quest Name: {quest.QuestName}, BoolClear: {quest.BoolClear}");
-        }
-    }
-
-    public QuestData GetQuest()
-    {
-        foreach (var quest in QuestDataDictionary.Values)
-        {
-            if (quest.BoolClear == false)
-            {
-                return quest;
-            }
-        }
-        return null;
     }
 }
