6ff3736c20dc2ea1840e77965b81da860bec17c4
이유신 <smdeks93@naver.com>
2024-06-10 18:34:20 +0900

    [chore] 머지 준비

--- Assets/Scripts/Objects/Eqiup/RightHandTool.cs
+++ Assets/Scripts/Objects/Eqiup/RightHandTool.cs
@@ -4,8 +4,6 @@ public class RightHandTool : EquipRightHand
 {
     [Header("Resource Gathering")]
     public bool doesGatherResources; //tag, 레이어 등으로 바꾸어 나무는 캐지고 돌은 안캐지는 식의 작업 필요.
-    public int gatherAmount; //자원을 얻는 양.
-    public float gatherRate; //자원이 나오는 빈도.
 
     protected override void PerformAttack()
     {
@@ -28,7 +26,12 @@ public class RightHandTool : EquipRightHand
             if (resourceCapacity != null)
             {
                 resourceCapacity.Hit();
+                Debug.Log("hit!!");
             }
+        }
+        else
+        {
+            Debug.Log("Error!!");
         }
     }
 }
