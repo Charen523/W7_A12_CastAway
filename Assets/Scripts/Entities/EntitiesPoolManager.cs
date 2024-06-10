fe3b140d498cbee13a205cd7eb6576e341732abb
JB1112 <124035533+JB1112@users.noreply.github.com>
2024-06-10 16:18:55 +0900

    [add] EntitiespoolManager 생성

---
+++ Assets/Scripts/Entities/EntitiesPoolManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class EntitiesPoolManager : Singleton<EntitiesPoolManager>
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+
+        [Header("DropInfo")]
+        public string itemName;
+
+        [Header("Position")]
+        public Vector3 positionMin;
+        public Vector3 positionMax;
+    }
+
+    public List<Pool> Pools;
+    public Dictionary<string, Queue<GameObject>> PoolDictionary;
+    public bool IsDead = false;
+
+    private Transform parentFolder;
+
+    public float respawnDelay = 10f;
+
+    // 오브젝트 위치를 저장할 딕셔너리
+    private Dictionary<GameObject, Vector3> originalPositions = new Dictionary<GameObject, Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlaceObjects();
+    }
+
+    public GameObject SpawnFromPool(string tag)
+    {
+        // 애초에 Pool이 존재하지 않는 경우
+        if (!PoolDictionary.ContainsKey(tag))
+            return null;
+
+        // 제일 오래된 객체를 재활용
+        GameObject obj = PoolDictionary[tag].Dequeue();
+        PoolDictionary[tag].Enqueue(obj);
+        obj.SetActive(true);
+        return obj;
+    }
+
+    // 오브젝트 배치 메서드
+    void PlaceObjects()
+    {
+        LayerMask terrainLayerMask = LayerMask.GetMask("Ground");
+
+        foreach (var pool in Pools)
+        {
+            for (int i = 0; i < pool.size; i++)
+            {
+                Vector3 offset = new Vector3(i * 0.01f, 0, i * 0.001f);
+                Vector3 position = new Vector3(
+                    Random.Range(pool.positionMin.x, pool.positionMax.x),
+                    30f,
+                    Random.Range(pool.positionMin.z, pool.positionMax.z)
+                );
+
+                GameObject obj = SpawnFromPool(pool.tag);
+                if (obj != null)
+                {
+                    RaycastHit hit;
+                    if (Physics.Raycast(position, Vector3.down, out hit, Mathf.Infinity, terrainLayerMask))
+                    {
+                        // y 위치를 지면에 맞추기
+                        position.y = hit.point.y;
+                    }
+                    else
+                    {
+                        // 레이캐스트 실패 시 기본 y 위치 사용
+                        position.y = pool.positionMin.y;
+                    }
+
+                    obj.transform.position = position + offset; // 객체 위치 설정(오프셋 더함)
+
+                    originalPositions[obj] = position; // 원래 위치 저장
+                }
+            }
+        }
+    }
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+        IsDead = false;
+        StartCoroutine(RespawnObject(obj, respawnDelay));
+    }
+
+    // 아이템 드랍 및 재생성 코루틴
+    private IEnumerator RespawnObject(GameObject obj, float delay)
+    {
+        Debug.Log("10초 후 생성");
+        yield return new WaitForSeconds(delay);
+        obj.SetActive(true);
+    }
+}
