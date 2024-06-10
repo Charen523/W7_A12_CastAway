45135305dae13fdc03b122f1ca172fbd260980aa
이유신 <smdeks93@naver.com>
2024-06-10 14:54:30 +0900

    [fix] 인벤토리 사라진 코드 추가

--- Assets/Scripts/UI/Workshop/Inventory/UIInventory.cs
+++ Assets/Scripts/UI/Workshop/Inventory/UIInventory.cs
@@ -20,7 +20,8 @@ public class UIInventory : MonoBehaviour
         USE_BTN,
         DROP_BTN,
         EQUIP_BTN,
-        UNEQUIP_BTN
+        UNEQUIP_BTN,
+        INSTALL_BTN
     }
 
     public event Action InventoryRefresh;
@@ -58,6 +59,7 @@ public class UIInventory : MonoBehaviour
         invenBtns[(int)eBtnIndex.DROP_BTN].GetComponent<Button>().onClick.AddListener(OnDropBtn);
         invenBtns[(int)eBtnIndex.EQUIP_BTN].GetComponent<Button>().onClick.AddListener(OnEquipBtn);
         invenBtns[(int)eBtnIndex.UNEQUIP_BTN].GetComponent<Button>().onClick.AddListener(OnUnequipBtn);
+        invenBtns[(int)eBtnIndex.INSTALL_BTN].GetComponent<Button>().onClick.AddListener(OnInstallBtn);
 
         /*slot 초기화.*/
         slots = new ItemSlot[holdings.childCount];
@@ -113,14 +115,14 @@ public class UIInventory : MonoBehaviour
         RemoveSelectedItem();
     }
 
-    public void OnEquipBtn()
+    private void OnEquipBtn()
     {
         if (selectedSlot.item is EquipData equipData)
         {
             //def는 플레이어의 스탯이므로 관련 처리 필요.
             equipData.isEquipped = true;
             //equipType에 따른 장비 슬롯에 장착하는 상호작용 필요.
-            CharacterManager.Instance.Player.equip.EquipNew(equipData);
+            //Playe.equip에서 장착했다고 알려야 함.
             //장착된 아이템임을 알리는 별도의 UI 필요.
 
             invenBtns[(int)eBtnIndex.EQUIP_BTN].SetActive(false);
@@ -128,13 +130,12 @@ public class UIInventory : MonoBehaviour
         }
     }
 
-    public void OnUnequipBtn()
+    private void OnUnequipBtn()
     {
         if (selectedSlot.item is EquipData equipData)
         {
             //def 스탯 원복 등 위의 주석내용 반대의 작업 필요.
             equipData.isEquipped = false;
-            CharacterManager.Instance.Player.equip.UnEquip();
 
             invenBtns[(int)eBtnIndex.EQUIP_BTN].SetActive(true);
             invenBtns[(int)eBtnIndex.UNEQUIP_BTN].SetActive(false);
@@ -156,7 +157,7 @@ public class UIInventory : MonoBehaviour
         }
     }
 
-    private void UpdateUI()
+    public void UpdateUI()
     {
         for (int i = 0; i < slots.Length; i++)
         {
@@ -237,9 +238,15 @@ public class UIInventory : MonoBehaviour
 
             invenBtns[(int)eBtnIndex.USE_BTN].SetActive(true);
         }
+        // 선택한 아이템타입이 건설이면, 설치 버튼 활성화
+        else if (selectedSlot.item.itemType == eItemType.Builts)
+        {
+            invenBtns[(int)eBtnIndex.INSTALL_BTN].SetActive(true);
+        }
         else
         {
             invenBtns[(int)eBtnIndex.USE_BTN].SetActive(false);
+            invenBtns[(int)eBtnIndex.INSTALL_BTN].SetActive(false);
 
             if (selectedSlot.item is EquipData equipItem)
             {
@@ -270,7 +277,7 @@ public class UIInventory : MonoBehaviour
         invenBtns[(int)eBtnIndex.DROP_BTN].SetActive(true);
     }
 
-    void RemoveSelectedItem()
+    public void RemoveSelectedItem()
     {
         selectedSlot.quantity--;
 
@@ -327,5 +334,31 @@ public class UIInventory : MonoBehaviour
         }
 
         UpdateUI();
+    }
+
+    private void OnInstallBtn()
+    {
+        CraftManager.Instance.CraftSystem.Install(selectedSlot.item);
+        RemoveSelectedItem();
+    }
+
+    public void RemoveItemByName(string itemID)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item != null && slots[i].item.itemId == itemID)
+            {
+                slots[i].quantity--;
+
+                if (slots[i].quantity <= 0)
+                {
+                    slots[i].item = null;
+                    ClearSelectedItemWindow();
+                }
+
+                UpdateUI();
+                break;
+            }
+        }
     }
 }
